0d151316482e5b59e70ee1c7b2af15a361a47e9f
MarcosOriolPago <marcos.oriol.p@gmail.com>
2025-12-16 18:31:06 +0100

    repo init

---
+++ src/plot_time_series.m
@@ -0,0 +1,12 @@
+function ax_handle = plot_time_series(array, fs)
+    time_vector = (1:length(array)) / fs;
+    disp(time_vector(end));
+    
+    plot(time_vector, array);
+    ax_handle = gca;
+    
+    xlabel(ax_handle, 'Time (s)');
+    ylabel(ax_handle, 'Amplitude (V)');
+    grid(ax_handle, 'on');
+    xlim([0 time_vector(end)])
+end
