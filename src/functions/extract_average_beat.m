295a87f3aa06bbeec48f7f2587bea2461cd9b045
MarcosOriolPago <marcos.oriol.p@gmail.com>
2025-12-20 18:05:06 +0100

    feat: finished report

--- src/functions/extract_average_beat.m
+++ src/functions/extract_average_beat.m
@@ -7,16 +7,10 @@ function avg_beat = extract_average_beat(signal, peak_locs, window_pre, window_p
         start_idx = peak_locs(i) - window_pre;
         end_idx = peak_locs(i) + window_post;
         
-        % Ensure indices are within bounds
         if start_idx > 0 && end_idx <= length(signal)
             segment = signal(start_idx:end_idx);
-            % Ensure segment is a row vector
             beat_segments = [beat_segments; segment(:)']; 
         end
-    end
-    
-    if isempty(beat_segments)
-        error('No valid segments extraction. Check peak_locs indices.');
     end
     
     avg_beat = mean(beat_segments, 1);
